d7670c4868e7b50448fa5ade80242ae8a94d3072
allon2005 <allon2005@gmail.com>
2016-06-23 23:37:43 -0500

    add JspServletBasic0

---
+++ JspServlet0/src/allon2005/servletBasic0/ServletBasic0.java
@@ -0,0 +1,42 @@
+package allon2005.servletBasic0;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletBasic0
+ */
+@WebServlet(description = "servlet basic0", urlPatterns = { "/ServletBasic0" }) // this class is annotated as "WebServlet" automatically. The "urlPatterns" is created during the class creation.
+                                                              // so, if the url request is: localHost:8080/JspServlet0/ServletBasic0, this class (servlet) will be called.
+public class ServletBasic0 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("this is doGet(..) from ServletBasic0");  //this will print the content to the console, not to the webpage (http://localhost:8080/JspServlet0/ServletBasic0 will be blank)
+		response.getWriter().append("hello from JspServlet0"); // using response.getWriter is to add the info to the response, which will be published to the webpage
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}*/
+
+}
+
+/*1. doGet(...) and doPost(..) take two parameters: request and response. We can extract info from request and put requested content to response, and then send 
+ * back to the client
+ * 
+ * 
+ */
